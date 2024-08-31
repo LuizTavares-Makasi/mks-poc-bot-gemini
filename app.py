1b53be3e6a124def8eac3fcf0068351c142e8349
Luiz Henrique Tavares <luiz.tavares@makasi.com.br>
2024-08-31 18:33:38 -0300

    Adiciona env ao .gitignore para ignorar arquivos de configuração locais

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 app = Flask(__name__)
 
 # Sets an environment variable GOOGLE_API_KEY with a specified API key.
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDV_TzW4k3dqapFmDLha2jh9BARbeUIWos"; 
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCKybiE4Jc8SGJ4sljig5qsSIaCAv9DUEk"; 
 
 # Defines a route for the home page (/) that sends the index.html file from the web directory.
 @app.route('/')
